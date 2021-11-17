b5533c08a384c42504540db4112eda9911726ab5
asolano1 <solanoandres.9595@gmail.com>
2021-11-17 00:11:43 -0600

    agregando unit tests para Tecnologia

---
+++ src/Test/Main/Tests/ProductoTest.java
@@ -0,0 +1,52 @@
+package Main.Tests;
+
+import Main.java.Producto;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+
+
+public class ProductoTest {
+
+    @Test
+    public void setDescripcion() {
+    }
+
+    @Test
+    public void getDescripcion() {
+    }
+
+    @Test
+    public void setCategoria() {
+    }
+
+    @Test
+    public void getCategoria() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setStock() {
+    }
+
+    @Test
+    public void getStock() {
+    }
+
+    @Test
+    public void setValor() {
+    }
+
+    @Test
+    public void getValor() {
+    }
+}
---
+++ src/Test/ProductoTest.java
@@ -0,0 +1,5 @@
+import Main.java.*;
+public class ProductoTest {
+
+
+}
