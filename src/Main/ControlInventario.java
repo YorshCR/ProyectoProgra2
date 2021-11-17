8620cf7c56bc0c3845177c62d1fe9259fcbc3c58
Jorge_SV <jorge.sanchez@contractor.brightinsight.com>
2021-11-16 22:09:57 -0600

    -correcciones menores a nombres de clases, -agrupacion de clases en package Main, -confirmar compilacion corrida correcta

---
+++ src/Main/ControlInventario.java
@@ -0,0 +1,7 @@
+
+
+public static void main String Args[]{
+
+        System.out.Println("");
+
+}
---
+++ src/Main/Producto.java
@@ -0,0 +1,48 @@
+public abstract class Producto {
+    protected String descripcion;
+    protected String categoria;
+    private int id;
+    private int stock;
+    private double valor;
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+}
